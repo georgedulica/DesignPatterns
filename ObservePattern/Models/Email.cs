e4409cbb58deab27238b350509e687328d4f690b
george.dulica <dulica_george_10@yahoo.com>
2023-11-01 17:42:57 +0200

    Add Observe Pattern

---
+++ ObservePattern/Models/Email.cs
@@ -0,0 +1,8 @@
+﻿namespace ObservePattern.Models;
+
+public class Email
+{
+    public string? EmailAddress { get; set; }
+
+    public string? Message { get; set; }
+}
---
+++ ObservePattern/Observables/Store.cs
@@ -0,0 +1,34 @@
+﻿using ObservePattern.Interfaces;
+using ObservePattern.Models;
+using ObservePattern.Observers;
+
+namespace ObservePattern.Observables;
+
+public class Store : IObservable
+{
+    private readonly IList<Email> customers;
+    private readonly IObserver notifyService;
+    public Store(IList<Email> customers, IObserver notifyService)
+    {
+        this.customers = customers;
+        this.notifyService = notifyService;
+    }
+
+    public void Subscribe(Email email)
+    {
+        customers.Add(email);
+    }
+
+    public void Unsubscribe(Email email)
+    {
+        customers.Remove(email);
+    }
+
+    public void Notify()
+    {
+        foreach (var customer in customers)
+        {
+            notifyService.Update(customer);
+        }
+    }
+}
