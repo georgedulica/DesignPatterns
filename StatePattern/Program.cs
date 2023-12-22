b9f66c5a9437097dd97da74d4b061c05d9553e6b
george.dulica <dulica_george_10@yahoo.com>
2023-12-22 12:51:30 +0200

    Add state pattern

--- StatePattern/Program.cs
+++ StatePattern/Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using StatePattern.Models;
+using StatePattern.StatePattern.State;
+
+public class Program
+{
+    static void Main()
+    {
+        var robot = new Robot();                            // state off
+        Console.WriteLine(robot.state.OnOnOff());           // state on
+        Console.WriteLine(robot.state.OnMoveStay());        // state move
+        Console.WriteLine(robot.state.OnOnOff());           // state off
+        Console.WriteLine(robot.state.OnMoveStay());        // state off
+        Console.WriteLine(robot.state.OnOnOff());           // state on
+        Console.WriteLine(robot.state.OnMoveStay());        // state move
+        Console.WriteLine(robot.state.OnMoveStay());        // state stay
+        Console.WriteLine(robot.state.OnOnOff());           // state off
+    }
+}
---
+++ StatePattern/StatePattern.State/StayState.cs
@@ -0,0 +1,29 @@
+﻿using StatePattern.Models;
+
+namespace StatePattern.StatePattern.State;
+
+public class StayState : State
+{
+    private readonly Robot robot;
+
+    public StayState(Robot robot) : base(robot)
+    {
+        this.robot = robot;
+    }
+
+    public override string OnMoveStay()
+    {
+        var message = robot.Move();
+        robot.setState(new MoveState(robot));
+
+        return message;
+    }
+
+    public override string OnOnOff()
+    {
+        var message = robot.TurnOff();
+        robot.setState(new OffState(robot));
+
+        return message;
+    }
+}
